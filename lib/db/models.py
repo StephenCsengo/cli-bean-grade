a0c0c766ed69fef3f5b61d7650d50b6777f2ffd2
Stephen Csengo <stephencsengo@gmail.com>
2023-08-23 10:56:34 -0400

    Add User model

--- lib/db/models.py
+++ lib/db/models.py
@@ -8,3 +8,13 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine("sqlite:///beangrade.db")
 
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+
+    def __repr__(self):
+        return f"User #{self.id}: " + f"{self.name}"
