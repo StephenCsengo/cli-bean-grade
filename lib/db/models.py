231ce378fda06676197889669f4c18b3e97503fc
Stephen Csengo <stephencsengo@gmail.com>
2023-08-23 11:21:30 -0400

    Add Coffee model

--- lib/db/models.py
+++ lib/db/models.py
@@ -18,3 +18,20 @@ class User(Base):
 
     def __repr__(self):
         return f"User #{self.id}: " + f"{self.name}"
+
+
+class Coffee(Base):
+    __tablename__ = "coffees"
+
+    id = Column(Integer(), primary_key=True)
+    roaster = Column(String(), index=True)
+    name = Column(String())
+    roast_level = Column(String(), index=True)
+    date_received = Column(DateTime(), default=datetime.now)
+
+    def __repr__(self):
+        return (
+            f"Coffee #{self.id}: "
+            + f"{self.roaster} {self.name}, "
+            + f"a {self.roast_level} roast received on {self.date_received}"
+        )
